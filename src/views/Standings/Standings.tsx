72be030b2371a036fd4860ef147c72939f12b58f
Dusan Odalovic <dodalovic@gmail.com>
2020-03-08 22:40:10 +0100

    styling

--- src/views/Standings/Standings.tsx
+++ src/views/Standings/Standings.tsx
@@ -14,7 +14,7 @@ function Standings(p: Props) {
     <Layout title="Standings">
       <div>
         <h1 className="text-indigo-700 text-center p-5">Standings</h1>
-        <div className="divisions flex">
+        <div className="divisions flex max-w-3xl m-auto">
           <Division name="East" />
           <Division name="West" />
         </div>
